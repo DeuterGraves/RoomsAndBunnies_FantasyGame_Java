e330f09b66527b5aa181a947fe90c9a283aa8ce1
DeuterGraves <carolinetgraves@gmail.com>
2018-11-29 13:20:53 +0000

    cleric built and tested

--- src/main/java/RoomsAndBunnies/Player/Cleric.java
+++ src/main/java/RoomsAndBunnies/Player/Cleric.java
@@ -4,12 +4,24 @@ import RoomsAndBunnies.Enums.Potion;
 
 public class Cleric extends Player {
 
-    private enum Potion potion;
+    private Potion potion;
     private int treasury;
 
     public Cleric(String name, String type, int healthPoints, Potion potion, int treasury){
         super(name, type, healthPoints);
         this.potion = potion;
         this.treasury = treasury;
+    }
+
+    public Potion getPotion() {
+        return this.potion;
+    }
+
+    public int getTreasury() {
+        return this.treasury;
+    }
+
+    public int getValueFromEnum(){
+        return this.potion.getPotionValue();
     }
 }
--- src/test/java/ClericTest.java
+++ src/test/java/ClericTest.java
@@ -1,6 +1,9 @@
 import RoomsAndBunnies.Enums.Potion;
 import RoomsAndBunnies.Player.Cleric;
 import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
 
 public class ClericTest {
 
@@ -10,7 +13,37 @@ public class ClericTest {
 
     @Before
     public void before(){
-        potion = new Potion.POTION1;
+        potion = Potion.POTION1;
         cleric = new Cleric("bob", "magic", 2000, potion, 0);
+    }
+
+    @Test
+    public void clericHasAName(){
+        assertEquals("bob", cleric.getName());
+    }
+
+    @Test
+    public void clericHasAType(){
+        assertEquals("magic", cleric.getType());
+    }
+
+    @Test
+    public void clericHasHealthPoints(){
+        assertEquals(2000, cleric.getHealthPoints());
+    }
+
+    @Test
+    public void clericHasAPotion(){
+        assertEquals(potion, cleric.getPotion());
+    }
+
+    @Test
+    public void clericHasATreasury(){
+        assertEquals(0, cleric.getTreasury());
+    }
+
+    @Test
+    public void potionHasAValue(){
+        assertEquals(20, cleric.getValueFromEnum());
     }
 }
