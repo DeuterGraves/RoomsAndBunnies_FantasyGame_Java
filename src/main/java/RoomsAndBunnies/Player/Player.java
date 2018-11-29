a5850e9958d940a43dadc8606b35e8841af32bc4
DeuterGraves <carolinetgraves@gmail.com>
2018-11-29 11:48:12 +0000

    player abstract class built

--- src/main/java/RoomsAndBunnies/Player/Player.java
+++ src/main/java/RoomsAndBunnies/Player/Player.java
@@ -1,4 +1,29 @@
 package RoomsAndBunnies.Player;
 
-public class Player {
+
+public abstract class Player {
+
+    protected String name;
+    protected String type;
+    protected int healthpoints;
+
+    public Player(String name, String type, int healthpoints){
+        this.name = name;
+        this.type = type;
+        this.healthpoints = healthpoints;
+    }
+
+    public String getName(){
+        return this.name;
+    }
+
+    public String getType(){
+        return this.type;
+    }
+
+    public int getHealthpoints(){
+        return this.healthpoints;
+    }
+
+
 }
