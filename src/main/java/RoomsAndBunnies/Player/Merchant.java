c765f4d6adf8eca5803b9772df4e93c08b411891
DeuterGraves <carolinetgraves@gmail.com>
2018-12-03 00:20:23 +0000

    merchant can haz inventory

--- src/main/java/RoomsAndBunnies/Player/Merchant.java
+++ src/main/java/RoomsAndBunnies/Player/Merchant.java
@@ -1,10 +1,33 @@
 package RoomsAndBunnies.Player;
 
+import RoomsAndBunnies.Weapons.Weapon;
+
+import java.util.HashMap;
+
 public class Merchant extends Player {
 
-
+    private HashMap<Weapon, Integer> inventory;
 
     public Merchant(String name, String type, int healthPoints){
         super(name, "Merchant", 800);
+        this.inventory = new HashMap<>();
     }
+
+    public HashMap<Weapon, Integer> getInventory() {
+        return inventory;
+    }
+
+    public void addWeaponToInventory(Weapon weapon, Integer count){
+        this.inventory.put(weapon, count);
+    }
+
+    public boolean checkInventory(Weapon weapon){
+        return this.inventory.containsKey(weapon);
+    }
+
+//    public int inventoryCount(){
+//        return inventory.containsKey()
+//    }
+
+
 }
---
+++ src/test/java/MerchantTest.java
@@ -0,0 +1,48 @@
+import RoomsAndBunnies.Enums.AxeType;
+import RoomsAndBunnies.Enums.ClubType;
+import RoomsAndBunnies.Enums.DragonType;
+import RoomsAndBunnies.Enums.PotionType;
+import RoomsAndBunnies.Player.Merchant;
+import RoomsAndBunnies.Weapons.Axe;
+import RoomsAndBunnies.Weapons.Club;
+import RoomsAndBunnies.Weapons.Dragon;
+import RoomsAndBunnies.Weapons.Potion;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class MerchantTest {
+
+    Merchant merchant;
+    Axe axe;
+    Club club;
+    Dragon dragon;
+    Potion potion;
+
+    @Before
+    public void before(){
+        axe = new Axe(5, AxeType.TOMAHAWK);
+        club = new Club(4, ClubType.AKLYS);
+        dragon = new Dragon(10, DragonType.PURPLE);
+        potion = new Potion(20, PotionType.POTION2);
+        merchant = new Merchant("Boris", "Merchant", 400 );
+    }
+
+    @Test
+    public void canAddWeaponToInventory(){
+        merchant.addWeaponToInventory(axe, 5);
+        assertEquals(true, merchant.checkInventory(axe));
+    }
+
+    @Test
+    public void canAddDifferentWeaponsToInventory(){
+        merchant.addWeaponToInventory(axe, 5);
+        merchant.addWeaponToInventory(dragon, 8);
+        merchant.addWeaponToInventory(potion, 11);
+        assertEquals(true, merchant.checkInventory(dragon));
+        assertEquals(false, merchant.checkInventory(club));
+    }
+
+
+}
